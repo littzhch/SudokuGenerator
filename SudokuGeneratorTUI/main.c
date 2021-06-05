87ca8a4a83f4e63a63b4f843098b5501d9b66f01
驰 张 <2371050115@qq.com>
2021-06-05 19:22:32 +0800

    更改gitignore

--- SudokuGeneratorTUI/main.c
+++ SudokuGeneratorTUI/main.c
@@ -44,7 +44,7 @@ int main2(void) {
 int main(void) {
 	int t1 = time(NULL);
 	PSUDOKUPUZZLE sp = malloc(sizeof(SUDOKUPUZZLE) * 200000);
-	GenerateSudokuMT(sp, 2000000, 31, 31, 64, 0);
+	GenerateSudokuMT(sp, 200000, 31, 31, 64, 1);
 	int t2 = time(NULL);
 	printf("%d\n", t2 - t1);
 }
